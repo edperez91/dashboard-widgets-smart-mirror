a4978bb5631c3120eb7e2956facfd1d6d45c1653
Edgar Perez <103350127+edperez91@users.noreply.github.com>
2022-11-08 22:57:07 -0800

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 // API key. Replace with your API key
-const APIKEY = 'f2f85762267ef18d0df71e18ec9f3737';
+const APIKEY = 'e05ec5a23c8bbd217673c10f7d07a236';
 // City
 const city = 'Spokane';
 // Units for Farenheit
