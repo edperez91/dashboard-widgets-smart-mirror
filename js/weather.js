0791cd53b10c99c0a21a79aef0983ab00f0af3d6
edperez91 <103350127+edperez91@users.noreply.github.com>
2022-11-10 13:09:14 -0800

    added webcam

--- js/weather.js
+++ js/weather.js
@@ -25,6 +25,7 @@ fetch(url)
     <data value="${data.name}" class="city">${data.name}</data>
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" alt="Placeholder">
+    <data value="${data.main.humidity}" class="humidity">${data.main.humidity}%\xa0\xa0Humidity</data>
   `;
   
   // Insert dynamic template to container
